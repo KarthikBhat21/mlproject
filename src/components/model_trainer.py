e96454125693e761330b1ca40992d54e1985f83e
Karthik Bhat <karthik.rbhat21@gmail.com>
2026-01-09 10:28:30 +0100

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression, Ridge,Lasso
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
@@ -43,20 +43,68 @@ class ModelTrainer:
                 )
             
             # Creating dictionary of models
-            models = {
-                    "Linear Regression": LinearRegression(),
-                    "Lasso": Lasso(),
-                    "Ridge": Ridge(),
-                    "K-Neighbors Regressor": KNeighborsRegressor(),
-                    "Decision Tree": DecisionTreeRegressor(),
-                    "Random Forest Regressor": RandomForestRegressor(),
-                    "XGBRegressor": XGBRegressor(), 
-                    "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                    "AdaBoost Regressor": AdaBoostRegressor()
-                }
+            models ={
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
             
-            model_report: dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test=y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter': ['best', 'random'],
+                    # 'max_features': ['sqrt', 'log2']
+                },
 
+                "Random Forest": {
+                    # 'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features': ['sqrt', 'log2', None],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                    # 'loss': ['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    # 'criterion': ['squared_error', 'friedman_mse'],
+                    # 'max_features': ['auto', 'sqrt', 'log2']
+                },
+
+                "Linear Regression": {},
+
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [5, 7, 9, 11]
+                    # 'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['ball_tree', 'kd_tree', 'brute']
+
+                },
+
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }    
+
+            }
+
+            model_report: dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models, params = params)
+            
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
